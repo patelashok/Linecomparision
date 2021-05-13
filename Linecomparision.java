498d99a37b0c82b405388fad7187f9049afbb40f
patelashok <ashok.patel2917@gmail.com>
2021-05-13 15:20:49 +0530

    Calculate length of Two lines'

--- Linecomparision.java
+++ Linecomparision.java
@@ -1,7 +1,25 @@
 
-
+import java.util.*;
 public class Linecomparision {
 	public static void main(String[] args){
 		System.out.println("Welcome to Line Comparison calculation");
-	}	
+		Scanner sc = new Scanner(System.in);
+		System.out.println(" Enter co-ordinates of first line: ");
+  	    double x1=sc.nextDouble();
+  	    double y1=sc.nextDouble();
+  	    double x2=sc.nextDouble();
+  	    double y2=sc.nextDouble();
+		double length1 = Math.sqrt((Math.pow((x2-x1),2)) + (Math.pow((y2-y1),2)));
+		System.out.println("The Length of Line 1 is : " + length1);
+		
+		System.out.println(" Enter co-ordinates of second line : ");
+  	    double a1=sc.nextDouble();
+  	    double b1=sc.nextDouble();
+  	    double a2=sc.nextDouble();
+  	    double b2=sc.nextDouble();
+
+  	    double length2 = Math.sqrt(Math.pow((a2-a1),2)+Math.pow((b2-b1),2));
+  	    System.out.println(" Length of line 2 is : " + length2);
+
+	}
 }
